aefc4762589eb8aeeead8dbfde758979e67ae77b
timothyclemansinsea <timothy.clemans@seattle.gov>
2015-11-11 23:41:21 -0800

    Update subscribe_to_every_change.py

--- subscribe_to_every_change.py
+++ subscribe_to_every_change.py
@@ -1,11 +1,21 @@
-# run with: ipython -i interactive_rethinkdb.py
 import rethinkdb as r
 conn = r.connect( "localhost", 28015).repl()
-db = r.db('opa')
-tables = db.table_list().run(conn)
-initial = db.table(tables[0]).changes().merge({'table':tables[0]})
 
-for table in tables[1:]:
-    initial = initial.union(db.table(table).changes().merge({'table':table}))
-for change in initial.run(conn):
-    print 'change', change
+def run_massive_cf():
+    dbs = r.db_list().run(conn)
+    tables = r.db(dbs[0]).table_list().run(conn)
+    initial = r.db(dbs[0]).table(tables[0]).changes().merge({'table':tables[0], 'database':dbs[0]})
+    for database in dbs:
+        tables = r.db(database).table_list().run(conn)    
+        for table in tables:
+            initial = initial.union(r.db(database).table(table).changes().merge({'table':table, 'database': database}))
+    for change in initial.run(conn):
+        print change
+        if change['database'] == 'rethinkdb' and change['table'] == 'table_config':
+            break
+        
+while True:
+    try:
+        run_massive_cf()
+    except:
+        pass
